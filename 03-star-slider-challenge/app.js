9fbb5650bd4753d03db40b2d1d18c74daf5d4923
Shanti Rai <shantirai@MacBookAir.lan>
2024-12-10 14:28:54 +0000

    wokred on task 3

--- 03-star-slider-challenge/app.js
+++ 03-star-slider-challenge/app.js
@@ -13,5 +13,53 @@ Expected Outcomes:
 */
 
 // Place your plan and solution below!
-//get star elements and create variable 
+
+// get star div container
+const starsContainer = document.getElementById('stars');
+
+//get star element/icon and create variable 
+const totalStars = document.querySelector('.star-icon');
 //get slider value and add to variable
+const slider = document.getElementById('starSlider');
+//console.log('slider.value:', slider.value)
+let sliderValue = slider.value;
+
+
+
+//create a function that sets the amount of stars equal to the value of the slider
+function changeStars() {
+
+  let sliderValue = slider.value;
+  
+  //create a new span element
+  let newSpan = document.createElement('span');
+  newSpan.appendChild(document.createTextNode('TEST'));
+
+  console.log('newSpan', newSpan)
+
+  
+
+  if (sliderValue === 1){
+    totalStars.textContent = 1;
+  } else if (sliderValue === 2){
+
+
+
+
+  } 
+  
+
+}
+
+
+//inside id stars add star icon span- add another span element
+//create a new element that is the new star icon
+// after creating we can append it to the element with the id 'stars'
+
+
+
+
+
+// clear on what type of event 'verb' e.g onchange this should be and set up an eventListender for when slider moves to update stars
+changeStars();
+slider.addEventListener('input', changeStars);
